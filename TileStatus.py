21bdeb28ff740f4abdb4d0bb908245c221315502
Jonah Wilson <jonahcwilson@gmail.com>
2017-08-14 20:26:01 -0400

    Changed TileStatus to constants

--- TileStatus.py
+++ TileStatus.py
@@ -1,34 +1,16 @@
 class TileStatus(object):
 
-# There are 5 possible statuses
-# 0: Neutral, unclaimed
-# 1: Fire
-# 2: Water
-# 3: Wind
-# 4: Earth
-#
-# 1-4 may also have up to three levels of strength which will all go into
-# changing a character's attributes.
-
-    def __init__(self, status, level):
-		self.status = status
-		self.level = level
-
-    # void
-    def Increment_Level(self):
-		if self.level < 3:
-	    	self.level += 1
-
-	# void
-    def Decrement_Level(self):
-		if self.level > 0:
-	    	self.level -= 1
-		else:
-	    	self.level = 0
-	    	self.status = 0
-
-	# void
-	def Change_Status(n):
-		self.status = n
-
+	NEUTRAL = 0
+	FIRE_ONE = 1
+	FIRE_TWO = 2
+	FIRE_THREE = 3
+	EARTH_ONE = 4
+	EARTH_TWO = 5
+	EARTH_THREE = 6
+	WIND_ONE = 7
+	WIND_TWO = 8
+	WIND_THREE = 9
+	WATER_ONE = 10
+	WATER_TWO = 11
+	WATER_THREE = 12
 
