ea9c241ee296d03ff243dd1b6057104089489cb0
Jonah Wilson <jonahcwilson@gmail.com>
2017-10-29 16:17:16 -0400

    Resetting Biome

--- GameTile.py
+++
@@ -1,6 +0,0 @@
-class GameTile(object):
-
-    def __init__(self, row, col, status):
-	self.status = status
-	self.row = row
-	self.col = col
--- Tile.py
+++
@@ -1,9 +0,0 @@
-class Tile(object):
-
-	def __init__(self, row, col, status):
-		self.status = status
-		self.row = row
-		self.col = col
-
-	def change_status(self, newStatus):
-		self.status = newStatus
