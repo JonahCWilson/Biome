172fff3e836e6a77976eb1e99f1d99ff0bcd08d6
Jonah Wilson <jonahcwilson@gmail.com>
2017-12-13 02:23:02 -0500

    Working on Player logic

--- Player.py
+++ Player.py
@@ -1,5 +1,8 @@
+from abc import ABCMeta
+
 class Player(object):
 
+	__metaclass__ = ABCMeta
 	'''
 	Player: abstract
 	Pos: (x, y)
@@ -10,12 +13,54 @@ class Player(object):
 	Spells: list
 	'''
 	
-	def init(self, Pos, HP, PP, XP, MV, Spells):
-		self.Pos = Pos
-		self.HP = HP
-		self.PP = PP
-		self.XP = XP
-		self.MV = MV
-		self.Spells = Spells
+		
+	def __init__(self, Position, HitPoints, PowerPoints, ExperiencePoints, MoveCount, Spells):
+		self.__Position = Position
+		self.__HitPoints = HitPoints
+		self.__PowerPoints = PowerPoints
+		self.__ExperiencePoints = ExperiencePoints
+		self.__MoveCount = MoveCount
+		self.__Spells = Spells
+		super().__init__()
+		
+		
+	def get_position(self):
+		return self.__Position
+	
+	def set_position(self, p):
+		self.__Position = p
+	
+	def get_hitpoints(self):
+		return self.__HitPoints
+		
+	def set_hitpoints(self, hp):
+		self.__HitPoints = hp
+		
+	def get_powerpoints(self):
+		return self.__PowerPoints
+		
+	def set_powerpoints(self, pp):
+		self.__PowerPoints = pp
+		
+	def get_experiencepoints(self):
+		return self.__ExperiencePoints
+		
+	def set_experiencepoints(self, xp):
+		self.__ExperiencePoints = xp
+	
+	def get_movecount(self):
+		return self.__MoveCount
+	
+	def set_movecount(self, moves):
+		self.__MoveCount = moves
+		
+	def get_spells(self):
+		return self.__Spells
+	
+	def set_spells(self, spells):
+		self.__Spells = spells
+	
+	def move(self):
+		pass
 		
 
---
+++ WaterPlayer.py
@@ -0,0 +1,9 @@
+from Player import Player
+class WaterPlayer(Player):
+	
+	def __init__(self):
+		super(0, 0, 0, 0, 0, []).__init__()
+		
+	def move(self):
+		self.set_position((0, 0))
+		
